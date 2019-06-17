8c9b5f6e5dc83cd4363b3b58f0f4c2ad3c50fe99
xiaodong tang <1297953037@qq.com>
2019-06-17 18:35:33 +0800

    add new project 追溯系统 客户端

---
+++ RetrospectiveSystem/project/RetrospectiveManager/RetrospectiveManager/SetStation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace RetrospectiveManager
+{
+    public partial class SetStation : Telerik.WinControls.UI.RadForm
+    {
+        public SetStation()
+        {
+            InitializeComponent();
+        }
+    }
+}
